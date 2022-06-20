6b1a18a4b527bf2f1878af0abf805f37b0792fc4
MarryDream <2037177696@qq.com>
2022-06-21 03:25:48 +0800

    feat: detail结构样式完成(图标除外)

--- components/chara-detail/app.js
+++ components/chara-detail/app.js
@@ -1,4 +1,4 @@
-const template = `<div class="chara-detail">
+const template = `<div class="chara-detail" :class="{ none: data.element === 'none' }">
 	<div class="portrait-box">
 		<img class="portrait" :src="portrait" alt="portrait">
 	</div>
@@ -33,8 +33,8 @@ const template = `<div class="chara-detail">
 		<div class="skill">
 			<p class="model-title">天赋</p>
 			<ul class="skill-content">
-				<li v-for="(s, sKey) of data.skill" :key="sKey">
-					<img :src="getSkillIcon(sKey)" :alt="'skill' + sKey + 1">
+				<li v-for="(s, sKey) of skills" :key="sKey">
+					<img :src="getSkillIcon(s.name)" :alt="s.name">
 					<div class="skill-level" :class="{ special: s.ext }">
 						<span>{{ s.level }}</span>
 					</div>
@@ -51,31 +51,68 @@ const template = `<div class="chara-detail">
 			</p>
 			<p class="fetter-info">好感度等级：{{ data.fetter }}</p>
 		</div>
+		<div class="artifact-list">
+			<artifact v-for="(a, aKey) of data.artifact.list" :position="aKey" :key="aKey" :data="a" />
+			<div class="extra-info">
+				<div class="artifact-effect card">
+					<i class="icon-flower bg-icon"></i>
+					<p v-for="(e, eKey) of data.artifact.effects">
+						<span>{{ e.count }}件套：</span>
+						<span>{{ e.name }}</span>
+					</p>
+				</div>
+				<div class="weapon-card card">
+					<i class="icon-weapon bg-icon"></i>
+					<div class="weapon-icon">
+						<img :src="weaponIcon" alt="data.weapon.name">
+						<p>{{ data.weapon.name }}</p>
+					</div>
+					<div class="weapon-info">
+						<p class="level-rank">
+							<span>Lv {{ data.weapon.level }}</span>
+							<span>R{{ data.weapon.affix }}</span>
+						</p>
+						<p class="star-list">
+							<img v-for="s in data.weapon.star" :key="s" src="https://mari-plugin.oss-cn-beijing.aliyuncs.com/image/common/star.png" alt="STAR">
+						</p>
+						<p class="weapon-attr">
+							<p v-for="(a, aKey) of data.weapon.attrs">
+								<i class="icon-flower"></i>
+								<span>{{ a.value }}</span>
+							</p>
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 	<div class="author">Create By Adachi-BOT</div>
 </div>`;
 
 import { request, parseURL } from "../../public/js/src.js";
+import Artifact from "./artifact.js";
 
 const { defineComponent, computed, onMounted } = Vue;
 
 export default defineComponent( {
 	name: "CharaDetailApp",
 	template,
-	components: {},
+	components: {
+		Artifact
+	},
 	setup() {
 		const urlParams = parseURL( location.search );
 		const data = request( `/api/chara?qq=${ urlParams.qq }` );
 		
 		const styleMap = {
-			anemo: [ "120deg", "120deg" ],
-			cryo: [ "165deg", "165deg" ],
-			dendro: [ "85deg", "85deg" ],
-			electro: [ "240deg", "240deg" ],
-			geo: [ "0deg", "0deg" ],
-			hydro: [ "170deg", "176deg" ],
-			pyro: [ "315deg", "315deg" ],
-			none: [ "0deg", "0deg" ]
+			anemo: [ "#41c88f", "110deg", "120deg" ],
+			cryo: [ "#41aac8", "150deg", "165deg" ],
+			dendro: [ "#51c841", "60deg", "70deg" ],
+			electro: [ "#6e41c8", "210deg", "225deg" ],
+			geo: [ "#c88b41", "-5deg", "0deg" ],
+			hydro: [ "#4176c8", "170deg", "176deg" ],
+			pyro: [ "#c84141", "310deg", "315deg" ],
+			none: [ "#999999", "0deg", "0deg" ]
 		}
 		
 		// 圣遗物默认图标
@@ -83,9 +120,20 @@ export default defineComponent( {
 		
 		onMounted( () => {
 			const elemStyle = styleMap[data.element];
-			document.documentElement.style.setProperty( "--bg-hue-rotate", elemStyle[0] );
-			document.documentElement.style.setProperty( "--talent-hue-rotate", elemStyle[1] );
+			document.documentElement.style.setProperty( "--base-color", elemStyle[0] );
+			document.documentElement.style.setProperty( "--bg-hue-rotate", elemStyle[1] );
+			document.documentElement.style.setProperty( "--talent-hue-rotate", elemStyle[2] );
 		} )
+		
+		/* 格式化技能 */
+		const skills = [];
+		for ( const skillKey in data.skill ) {
+			if ( skillKey === "undefined" ) continue;
+			skills.push( {
+				...data.skill[skillKey],
+				name: skillKey
+			} )
+		}
 		
 		/* 给圣遗物添加种类图标 */
 		const artifacts = computed( () => data.artifacts.list.map( ( a, aKey ) => {
@@ -94,6 +142,8 @@ export default defineComponent( {
 				typeIcon: artifactsFontIcon[aKey]
 			}
 		} ) )
+		
+		const weaponIcon = computed( () => `https://adachi-bot.oss-cn-beijing.aliyuncs.com/Version2/thumb/weapon/${ data.weapon.name }.png` )
 		
 		const userAvatar = computed( () => `https://q1.qlogo.cn/g?b=qq&s=640&nk=${ urlParams.qq }` );
 		
@@ -115,6 +165,8 @@ export default defineComponent( {
 			urlParams,
 			userAvatar,
 			portrait,
+			skills,
+			weaponIcon,
 			getTalentIcon,
 			getSkillIcon
 		}
---
+++ components/chara-detail/artifact.js
@@ -0,0 +1,58 @@
+const template = `<div class="artifact card">
+	<template v-if="!isEmpty">
+		<div class="artifact-base">
+			<img :src="artifactIcon" :alt="data.artifactName">
+			<div class="artifact-base-info">
+				<p class="artifact-name">{{ data.artifactName }}</p>
+				<p class="artifact-info">
+					<span class="level">+{{ data.level }}</span>
+					<p class="star-list">
+						<img v-for="s of data.rank" :key="s" src="https://mari-plugin.oss-cn-beijing.aliyuncs.com/image/common/star.png" alt="STAR">
+					</p>
+				</p>
+			</div>
+		</div>
+		<div class="list-item special">
+			<p class="attr-label">{{ data.mainAttr.attr }}</p>
+			<p class="attr-label-value">{{ data.mainAttr.value }}</p>
+		</div>
+		<div v-for="(sub, subKey) of data.subAttr" :key="subKey" class="list-item">
+			<p class="attr-label">{{ sub.attr }}</p>
+			<p class="attr-label-value">{{ sub.value }}</p>
+		</div>
+	</template>
+	<div class="is-empty" v-else>
+		<span>EMPTY</span>
+	</div>
+</div>`
+
+const { defineComponent, computed } = Vue;
+
+export default defineComponent( {
+	name: "CharaDetailArtifact",
+	template,
+	props: {
+		data: {
+			type: Object
+		},
+		position: {
+			type: String
+		}
+	},
+	setup( props ) {
+		const data = props.data;
+		const position = props.position;
+		
+		const isEmpty = computed( () => !data || Object.keys( data ).length === 0 );
+		
+		const artifactIcon = computed( () => {
+			if ( !data ) return "";
+			return `https://adachi-bot.oss-cn-beijing.aliyuncs.com/Version2/artifact/${ data.shirtId }/${ position }.png`;
+		} )
+		
+		return {
+			isEmpty,
+			artifactIcon
+		}
+	}
+} )
